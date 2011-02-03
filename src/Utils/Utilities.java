8bb619408302bb1ba73e8debad83929879c5a23e
Bryan Harris <webmaster@wmdracing.net>
2011-02-03 01:43:07 -0800

    Added Utilities.java to git repo... totally forgot

---
+++ src/Utils/Utilities.java
@@ -0,0 +1,43 @@
+/* DataReader
+ *
+ * Copyright 2011
+ *
+ * This file is part of the DataReader project.
+ *
+ * DataReader software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DataReader software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with any DataReader software.  If not, see http://www.gnu.org/licenses/
+ *
+ * I ask that if you make any changes to this file you fork the code on github.com!
+ *
+ */
+
+package Utils;
+
+import java.io.File;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Utilities {
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 && i < s.length() - 1) {
+            ext = s.substring(i + 1).toLowerCase();
+        }
+        return ext;
+    }
+}
