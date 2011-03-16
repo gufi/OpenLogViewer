292a6500b2364ffa96915e170e6e9ccb7ce1e46a
Fred Cooke <fred.cooke@gmail.com>
2011-03-17 00:56:59 +1300

    Fixed up pom slightly to allow mvn exec:exec to run the app and added some bogus unit tests, two of which pass and one of which fails. Run with mvn test. Might do more tomorrow, but basics work now. Need to create parent project and release to nexus once dcramer sets it up, and then refactor freeems bin parser heavily and cover with thorough unit tests, and then split it out into it's own project for use in other apps.

---
+++ src/test/java/org/diyefi/openlogviewer/decoder/FreeEMSBinaryTest.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.diyefi.openlogviewer.decoder;
+
+import junit.framework.TestCase;
+
+/**
+ * @author fred
+ *
+ */
+public class FreeEMSBinaryTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public FreeEMSBinaryTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testChecksum(){
+		assertEquals(true, true);
+	}
+	
+	public void testFail(){
+		assertTrue(false);
+	}
+	
+	public void testPass(){
+		assertTrue(true);
+	}
+
+}
