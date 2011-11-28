0660e895ce660bfb415e9fd2b3c07deccc6b4f4d
Benjamin Fenner <fenfam@sc.rr.com>
2011-11-28 11:00:48 -0500

    Mantis issue #446. Start/end marking is done by arrows pointing inward. Other styles of start/end marking will also be tried and we will pick the best style. Maybe this will be configurable in the future.

--- src/main/java/org/diyefi/openlogviewer/graphing/SingleGraphPanel.java
+++ src/main/java/org/diyefi/openlogviewer/graphing/SingleGraphPanel.java
@@ -141,13 +141,15 @@ public class SingleGraphPanel extends JPanel implements HierarchyBoundsListener,
 			// Setup graph states and draw graph beginning and end markers
 			if(leftOfTraceData == -Double.MAX_VALUE && traceData != -Double.MAX_VALUE){
 				// At graph beginning
-				g2d.drawLine(screenPositionXCoord - 2, screenPositionYCoord - 2, screenPositionXCoord - 2, screenPositionYCoord + 2);
+				g2d.drawLine(screenPositionXCoord - 4, screenPositionYCoord - 2, screenPositionXCoord - 2, screenPositionYCoord);
+				g2d.drawLine(screenPositionXCoord - 2, screenPositionYCoord, screenPositionXCoord - 4, screenPositionYCoord + 2);
 				atGraphBeginning = true;
 				insideGraph = true;
 			}
 			if(traceData != -Double.MAX_VALUE && rightOfTraceData == -Double.MAX_VALUE){
 				// At graph end
-				g2d.drawLine(screenPositionXCoord + 2, screenPositionYCoord - 2, screenPositionXCoord + 2, screenPositionYCoord + 2);
+				g2d.drawLine(screenPositionXCoord + 4, screenPositionYCoord - 2, screenPositionXCoord + 2, screenPositionYCoord);
+				g2d.drawLine(screenPositionXCoord + 2, screenPositionYCoord, screenPositionXCoord + 4, screenPositionYCoord + 2);
 				atGraphEnd = true;
 			}
 
